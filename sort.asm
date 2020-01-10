62483da3bd2f85d18edff4740a53443326b86192
yashk2000 <yashsja@gmail.com>
2020-01-11 01:50:35 +0530

    Sorting

---
+++ sort.asm
@@ -0,0 +1,20 @@
+MVI B, 09                     
+START: 	LXI H, 2200H
+	MVI C, 09H 
+               
+	SORT:   MOV A, M                
+		INX H                       
+		CMP M               
+		JC NEXT                
+		JZ NEXT               
+		MOV D, M
+		MOV M, A
+	    	DCX H
+		MOV M, D
+		INX H  
+		            
+	NEXT:	DCR C                       
+		JNZ SORT               
+		DCR B                
+		JNZ START
+HLT                        
